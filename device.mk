30dd30d4075139b8d2fbe1669220d595631ffd7b
Davide Garberi <dade.garberi@gmail.com>
2023-06-16 01:11:03 +0200

    beryllium: Build btconfigstore 2.0 vendor library
    Change-Id: I762cbb3c2c3817e83310f7033ff006f72ee41794

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2246
