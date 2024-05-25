30f6e5dec41475fcc34769c1fadf3dcda029729a
SonicBSV <sonicbsv@gmail.com>
2024-05-25 19:25:27 +0300

    beryllium: add android.hardware.bluetooth@1.1

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
