df5d74943fc1b02f914f3e878c6343bb72d9c7de
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    beryllium: Include AOSP FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,8 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/aosp/config/device_framework_matrix.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
