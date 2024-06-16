ba8b5bfc2753dd8a2acdfd03bf3822f79f7d7b4b
Bruno Martins <bgcngm@gmail.com>
2024-06-16 15:14:57 +0100

    beryllium: Inherit from QTI and common Xiaomi FCMs
    Change-Id: I8d4d54bba1b3dff27c11b0034852697b818ee609

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,8 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Kernel
