079ec883a707ab8b24ede27ff501e8ce9cf5344a
ihklknz <haikalluthfianinobalukia@proton.me>
2024-06-28 16:31:27 +0000

    beryllium: Nuke Some Packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Calendar Maps Velvet
+LOCAL_OVERRIDES_PACKAGES := Maps Velvet NgaResources PrebuiltGmail MarkupGoogle SafetyHubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
