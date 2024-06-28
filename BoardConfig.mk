8fb9d92b48751b8855225a74b91057d2ab3e562d
xiaomimidofan <midooneteam@gmail.com>
2024-06-28 09:31:25 +0000

    beryllium: drop custom clang *Also change defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,10 +69,7 @@ BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc androidboot.i
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
-TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM_IAS=1
-TARGET_KERNEL_CLANG_VERSION := custom
-TARGET_KERNEL_CONFIG += vendor/xiaomi/mi845_defconfig vendor/xiaomi/beryllium.config
+TARGET_KERNEL_CONFIG := beryllium_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
 # LMKD
