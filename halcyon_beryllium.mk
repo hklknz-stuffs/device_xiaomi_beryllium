6b61e1e9f83f23e604eaca14e99889cabaaa56f9
haikaru17 <haikalluthfianinobalukia@proton.me>
2024-08-22 09:06:35 +0000

    beryllium: get enchanted.

--- halcyon_beryllium.mk
+++ halcyon_beryllium.mk
@@ -10,13 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
+-include vendor/extra/product.mk
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := halcyon_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCOPHONE F1
